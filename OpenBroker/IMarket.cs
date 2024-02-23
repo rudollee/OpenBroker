a16efaa6c0e521230ea27ee3b6e7d1aecd6dae8d
rudollee <rudollee@gmail.com>
2024-02-23 23:14:29 +0900

    OpenBroker IMarket specification

--- OpenBroker/IMarket.cs
+++ OpenBroker/IMarket.cs
@@ -7,8 +7,16 @@ using OpenBroker.Models;
 
 namespace OpenBroker;
 
+/// <summary>
+/// Market Data
+/// </summary>
 public interface IMarket
 {
+	/// <summary>
+	/// Marekt Contracted callback
+	/// </summary>
+	EventHandler<ResponseResult<MarketContract>> MarketContracted { get; set; }
+
 	/// <summary>
 	/// Market Depth callback
 	/// </summary>
@@ -18,4 +26,42 @@ public interface IMarket
 	/// News callback
 	/// </summary>
 	EventHandler<ResponseResult<News>> NewsPosted { get; set; }
+
+	/// <summary>
+	/// 현재가
+	/// </summary>
+	/// <param name="symbol"></param>
+	/// <returns></returns>
+	Task<ResponseResult<MarketContract>> RequestMarketContract(string symbol);
+
+	/// <summary>
+	/// 현재가 - realtime
+	/// </summary>
+	/// <param name="symbol"></param>
+	/// <param name="connecting"></param>
+	/// <returns></returns>
+	Task<ResponseCore> SubscribeMarketContract(string symbol, bool connecting = true);
+
+	/// <summary>
+	/// 호가 - realtime
+	/// </summary>
+	/// <param name="symbol"></param>
+	/// <param name="connecting"></param>
+	/// <returns></returns>
+	Task<ResponseCore> SubscribeMarketDepth(string symbol, bool connecting = true);
+
+	/// <summary>
+	/// 뉴스 정보 - realtime
+	/// </summary>
+	/// <param name="connecting"></param>
+	/// <returns></returns>
+	Task<ResponseCore> SubscribeNews(bool connecting = true);
+
+	/// <summary>
+	/// 종목 상세
+	/// </summary>
+	/// <param name="symbol"></param>
+	/// <returns></returns>
+	Task<ResponseResult<Instrument>> RequestInstrumentInfo(string symbol);
+
 }
