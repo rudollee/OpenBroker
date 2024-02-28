c49cdfbad14781a831f79786159614fe4c5d2dc4
rudollee <rudollee@gmail.com>
2024-02-28 20:00:52 +0900

    KisKrxEquity Init

---
+++ KisOpenApi/KrxEquity/KisKrxEquityConnection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenBroker;
+using OpenBroker.Models;
+using Websocket.Client;
+
+namespace KisOpenApi.KrxEquity;
+public partial class KisKrxEquity : ConnectionBase, IConnection
+{
+	public void SetKeyPack(KeyPack keyInfo) => _keyInfo = keyInfo;
+	public KeyPack KeyInfo { get => _keyInfo; }
+	private KeyPack _keyInfo = new KeyPack();
+
+	public void SetAccount(Account account) => _accountInfo = account;
+	public Account AccountInfo { get => _accountInfo; }
+	private Account _accountInfo = new Account();
+
+	public void SetBankAccount(BankAccount bankAccount) => _bankAccountInfo = bankAccount;
+	public BankAccount BankAccountInfo { get => _bankAccountInfo; }
+	private BankAccount _bankAccountInfo = new BankAccount();
+
+	public bool IsConnected { get => _connected; }
+	private bool _connected = false;
+
+	private IWebsocketClient client;
+
+	private string _iv = string.Empty;
+	private string _key = string.Empty;
+
+	public Task<ResponseCore> ConnectAsync() => throw new NotImplementedException();
+	public Task<ResponseCore> DisconnectAsync() => throw new NotImplementedException();
+	public Task<ResponseResult<KeyPack>> RequestAccessTokenAsync(string appkey, string appsecret) => throw new NotImplementedException();
+	public Task<ResponseResult<KeyPack>> RequestApprovalKeyAsync(string appkey, string secretkey) => throw new NotImplementedException();
+}
---
+++ KisOpenApi/KrxEquity/KisKrxEquityMarket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenBroker;
+using OpenBroker.Models;
+
+namespace KisOpenApi.KrxEquity;
+public partial class KisKrxEquity : ConnectionBase, IMarket
+{
+	public EventHandler<ResponseResult<MarketContract>> MarketContracted { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+	public EventHandler<ResponseResult<MarketDepth>> MarketDepthListed { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+	public EventHandler<ResponseResult<News>> NewsPosted { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+	public Task<ResponseResult<Instrument>> RequestInstrumentInfo(string symbol) => throw new NotImplementedException();
+	public Task<ResponseResult<MarketContract>> RequestMarketContract(string symbol) => throw new NotImplementedException();
+	public Task<ResponseCore> SubscribeMarketContract(string symbol, bool connecting = true) => throw new NotImplementedException();
+	public Task<ResponseCore> SubscribeMarketDepth(string symbol, bool connecting = true) => throw new NotImplementedException();
+	public Task<ResponseCore> SubscribeNews(bool connecting = true) => throw new NotImplementedException();
+}
