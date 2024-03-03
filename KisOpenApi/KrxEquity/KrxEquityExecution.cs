4e7553440eff670de575bc632ca7b50efb72d6f6
rudollee <rudollee@gmail.com>
2024-03-04 02:09:26 +0900

    Kis krx equity order buy/sell/cancle implemented

--- KisOpenApi/KrxEquity/KrxEquityExecution.cs
+++ KisOpenApi/KrxEquity/KrxEquityExecution.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KisOpenApi.Models.GlobalFutures;
+using KisOpenApi.Models.KrxEquity;
 using OpenBroker;
 using OpenBroker.Models;
+using RestSharp;
 
 namespace KisOpenApi.KrxEquity;
 public partial class KisKrxEquity : ConnectionBase, IExecution
@@ -13,8 +17,87 @@ public partial class KisKrxEquity : ConnectionBase, IExecution
 	public EventHandler<ResponseResult<Order>> Executed { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 	public EventHandler<ResponseResult<Balance>> BalanceAggregated { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
-	public Task<ResponseCore> AddOrderAsync(OrderCore order) => throw new NotImplementedException();
-	public Task<ResponseCore> CancelOrderAsync(DateOnly bizDate, long oid, int volume) => throw new NotImplementedException();
+
+	#region 국내주식 주문 매수/매도/정정/취소 TTTC0802U/TTTC0801U/TTTC0803U 
+	public async Task<ResponseCore> AddOrderAsync(OrderCore order)
+	{
+		if (string.IsNullOrEmpty(BankAccountInfo.AccountNumber)) return new ResponseCore
+		{
+			StatusCode = Status.UNAUTHORIZED,
+			Code = "ANUMBER",
+			Message = "no account number"
+		};
+
+		var parameters = GenerateParameters(new
+		{
+			PDNO = order.Symbol,
+			ORD_DVSN = order.OrderType == OrderType.MARKET ? "01" : "00",
+			ORD_QTY = order.VolumeOrdered,
+			ORD_UNPR = order.OrderType == OrderType.MARKET ? "0" : order.PriceOrdered.ToString(),
+		});
+
+		return await RequestOrderAsync(order.IsLong ? "TTTC0802U" : "TTTC0801U", parameters);
+	}
+
+	public Task<ResponseCore> UpdateOrderAsync(OrderCore order) => throw new NotImplementedException();
+	
+	public async Task<ResponseCore> CancelOrderAsync(DateOnly bizDate, long oid, int volume)
+	{
+		if (oid == 0 || volume == 0) return new ResponseCore
+		{
+			StatusCode = Status.BAD_REQUEST,
+			Code = "REFUSE",
+			Message = "one of parameter value is not correct.",
+		};
+
+		var parameters = GenerateParameters(new
+		{
+			KRX_FWDG_ORD_ORGNO = "",
+			ORGN_ODNO = oid.ToString().PadLeft(10, '0'),
+			ORD_DVSN = "00",
+			RVSE_CNCL_DVSN_CD = "02",
+			ORD_QTY = "0",
+			ORD_UNPR = "0",
+			QTY_ALL_ORD_YN = "Y"
+		});
+
+		return await RequestOrderAsync("TTTC0803U", parameters);
+	}
+
+	private async Task<ResponseCore> RequestOrderAsync(string trId, object parameters)
+	{
+		var client = new RestClient($"{host}/uapi/domestic-stock/v1/trading/order-{(trId != "TTTC0803U" ? "cash" : "-rvsecncl")}");
+		var request = new RestRequest().AddHeaders(GenerateHeaders(trId)).AddBody(parameters);
+
+		try
+		{
+			var response = await client.PostAsync<TTTC080XU>(request);
+			if (response is null || response.Output is null || response.ResultCode != "0") return new ResponseCore
+			{
+				StatusCode = Status.ERROR_OPEN_API,
+				Code = response?.ResponseCode ?? "NULL",
+				Message = response?.ResponseMessage ?? "response is null",
+			};
+
+			return new ResponseCore
+			{
+				StatusCode = Status.SUCCESS,
+				Code = response.ResponseCode,
+				Message = response.ResponseMessage,
+				Remark = $"{response.Output.OrderTime6}-{response.Output.OrderNumber}"
+			};
+		}
+		catch (Exception ex)
+		{
+			return new ResponseCore
+			{
+				StatusCode = Status.INTERNALSERVERERROR,
+				Message = $"error catch: {ex.Message}"
+			};
+		}
+	}
+	#endregion
+
 	public Task<ResponseResult<Balance>> RequestBalancesAsync(DateTime? date = null) => throw new NotImplementedException();
 	public Task<ResponseResults<Contract>> RequestContractsAsync(ContractStatus status = ContractStatus.ExecutedOnly) => throw new NotImplementedException();
 	public Task<ResponseResults<Contract>> RequestContractsAsync(DateTime dateBegun, DateTime dateFin, ContractStatus status = ContractStatus.ExecutedOnly) => throw new NotImplementedException();
@@ -25,5 +108,61 @@ public partial class KisKrxEquity : ConnectionBase, IExecution
 	public Task<ResponseResults<Position>> RequestPositionsAsync(DateTime? date = null) => throw new NotImplementedException();
 	public Task<ResponseCore> SubscribeContractAsync(bool connecting = true) => throw new NotImplementedException();
 	public Task<ResponseCore> SubscribeOrderAsync(bool connecting = true) => throw new NotImplementedException();
-	public Task<ResponseCore> UpdateOrderAsync(OrderCore order) => throw new NotImplementedException();
+
+	/// <summary>
+	/// Generate Parameters
+	/// </summary>
+	/// <param name="additionalOption"></param>
+	/// <returns></returns>
+	private Dictionary<string, string?> GenerateParameters(Dictionary<string, string?> additionalOption)
+	{
+		var basicParameters = new
+		{
+			CANO = BankAccountInfo.AccountNumber.Substring(0, 8),
+			ACNT_PRDT_CD = BankAccountInfo.AccountNumber.Substring(8),
+		};
+
+		var parameters = basicParameters.GetType().GetProperties().ToDictionary(x => x.Name, x => x.GetValue(basicParameters, null)?.ToString());
+		foreach (var parameter in additionalOption)
+		{
+			parameters.Add(parameter.Key, parameter.Value?.ToString());
+		}
+
+		return parameters ?? new Dictionary<string, string?>();
+	}
+
+	/// <summary>
+	/// Generate QueryParameters
+	/// </summary>
+	/// <param name="additionalOption"></param>
+	/// <returns></returns>
+	private Dictionary<string, string?> GenerateParameters(object additionalOption) =>
+		GenerateParameters(additionalOption.GetType().GetProperties().ToDictionary(x => x.Name, x => x.GetValue(additionalOption, null)?.ToString()));
+
+	/// <summary>
+	/// Generate Headers
+	/// </summary>
+	/// <param name="tr"></param>
+	/// <returns></returns>
+	private Dictionary<string, string> GenerateHeaders(string tr)
+	{
+		return new Dictionary<string, string>
+		{
+			{ "content-type", "application/json" },
+			{ "authorization", $"Bearer {KeyInfo.AccessToken}"},
+			{ "appkey", KeyInfo.AppKey },
+			{ "appsecret", KeyInfo.SecretKey},
+			{ "tr_id", tr},
+			{ "custtype", "P" }
+		};
+	}
+
+	/// <summary>
+	/// Generate Headers
+	/// </summary>
+	/// <param name="tr"></param>
+	/// <param name="additionalOption"></param>
+	/// <returns></returns>
+	private Dictionary<string, string> GenerateHeaders(string tr, object additionalOption) =>
+		GenerateHeaders(tr, additionalOption.GetType().GetProperties().ToDictionary(x => x.Name, x => x.GetValue(additionalOption, null)?.ToString()));
 }
---
+++ KisOpenApi/Models/KrxEquity/Order.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace KisOpenApi.Models.KrxEquity;
+/// <summary>
+/// 국내주식 - 주문 신규/정정/취소 공통
+/// </summary>
+internal class TTTC080XU : ExecutionBaseResponse
+{
+	public TTTC080XUOutput Output { get; set; } = new();
+}
+
+/// <summary>
+/// 국내주식 TTTC080XU Output
+/// </summary>
+internal class TTTC080XUOutput
+{
+	/// <summary>
+	/// 한국거래소전송주문조직번호
+	/// </summary>
+	[JsonPropertyName("KRX_FWDG_ORD_ORGNO")]
+	public string KrxFwdgOrdOrgno { get; set; } = string.Empty;
+
+	/// <summary>
+	/// 주문시각
+	/// </summary>
+	[JsonPropertyName("ORD_TMD")]
+	public string OrderTime6 { get; set; } = string.Empty;
+
+	/// <summary>
+	/// 주문번호
+	/// </summary>
+	[JsonPropertyName("ODNO")]
+	public string OrderNumber { get; set; } = string.Empty;
+}
