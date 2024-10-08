4ec205f515ed522a7a6ddb349f4ab6ce7896e927
rudollee <rudollee@gmail.com>
2024-10-09 03:31:53 +0900

    specification change followed

--- KisOpenApi/GlobalFutures/GlobalFuturesMarket.cs
+++ KisOpenApi/GlobalFutures/GlobalFuturesMarket.cs
@@ -76,10 +76,10 @@ public partial class KisGlobalFutures : ConnectionBase, IMarket
 	public Task<ResponseResults<Instrument>> RequestInstruments(int option) => throw new NotImplementedException();
 	public Task<ResponseResult<MarketContract>> RequestMarketContract(string symbol) => throw new NotImplementedException();
 	public Task<ResponseResult<News>> RequestNews(string id) => throw new NotImplementedException();
-	public async Task<ResponseCore> SubscribeMarketContract(string symbol, bool connecting = true) =>
+	public async Task<ResponseCore> SubscribeMarketContract(string symbol, bool connecting = true, string subscriber = "") =>
 		await Subscribe(nameof(HDFFF020), symbol, connecting);
 
-	public async Task<ResponseCore> SubscribeMarketDepth(string symbol, bool connecting = true) =>
+	public async Task<ResponseCore> SubscribeMarketDepth(string symbol, bool connecting = true, string subscriber = "") =>
 		await Subscribe(nameof(HDFFF010), symbol, connecting);
 	public Task<ResponseCore> SubscribeNews(bool connecting = true) => throw new NotImplementedException();
 	public Task<ResponseResults<MarketContract>> RequestMarketContract(List<string> symbols) => throw new NotImplementedException();
