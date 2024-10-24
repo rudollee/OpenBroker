4faf17c2b1041f8fa6f315810f81b0dde59fdd3b
rudollee <rudollee@gmail.com>
2024-10-25 02:03:54 +0900

    specification updated : News

--- OpenBroker/Models/News.cs
+++ OpenBroker/Models/News.cs
@@ -4,7 +4,8 @@ public class News
 {
     public required string Code { get; set; }
     public DateTime TimePosted { get; set; }
-    public string Media { get; set; } = string.Empty;
+    public int PublisherId { get; set; }
+    public string Publisher { get; set; } = string.Empty;
     public required string Title { get; set; }
     public string Body { get; set; } = string.Empty;
     public string Remark { get; set; } = string.Empty;
