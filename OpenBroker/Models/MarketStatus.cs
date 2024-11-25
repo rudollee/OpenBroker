e32c6b72a6f25b7d6da1922b5aba83aaacd3b434
rudollee <rudollee@gmail.com>
2024-11-26 00:42:10 +0900

    specification updated : MarketStatus model removed

--- OpenBroker/Models/MarketStatus.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace OpenBroker.Models;
-
-public class MarketStatus
-{
-    public MarketCode MarketCat { get; set; }
-    public string Code { get; set; } = string.Empty;
-    public string Message { get; set; } = string.Empty;
-}
