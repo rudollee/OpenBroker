bdba468df15e971bd1c16f263c9aa8926e20f895
rudollee <rudollee@gmail.com>
2025-03-18 16:05:08 +0900

    T1866, T1859 model added

---
+++ LsOpenApi/Models/T1859.cs
@@ -0,0 +1,61 @@
+﻿namespace LsOpenApi.Models;
+/// <summary>
+/// 서버저장조건 조건검색
+/// </summary>
+internal class t1859 : LsResponseCore
+{
+	public t1859InBlock t1857InBlock { get; set; } = new();
+	public t1859OutBlock t1857OutBlock { get; set; } = new();
+	public List<t1859OutBlock1> t1857OutBlock1 { get; set; } = new();
+}
+
+/// <summary>
+/// 서버저장조건 조건검색 - InBlock
+/// </summary>
+internal class t1859InBlock
+{
+	/// <summary>종목검색입력값</summary>
+	public string query_index { get; set; } = string.Empty;
+}
+
+/// <summary>
+/// 서버저장조건 조건검색 - OutBlock
+/// </summary>
+internal class t1859OutBlock
+{
+	/// <summary>검색종목수</summary>
+	public long result_count { get; set; }
+
+	/// <summary>포착시간</summary>
+	public string result_time { get; set; } = string.Empty;
+
+	/// <summary>전략설명</summary>
+	public string text { get; set; } = string.Empty;
+}
+
+/// <summary>
+/// 서버저장조건 조건검색 - OutBlock1
+/// </summary>
+internal class t1859OutBlock1
+{
+	/// <summary>종목코드</summary>
+	public string shcode { get; set; } = string.Empty;
+
+	/// <summary>종목명</summary>
+	public string hname { get; set; } = string.Empty;
+
+	/// <summary>현재가</summary>
+	public long price { get; set; }
+
+	/// <summary>전일대비구분</summary>
+	public string sign { get; set; } = string.Empty;
+
+	/// <summary>전일대비</summary>
+	public long change { get; set; }
+
+	/// <summary>등락율</summary>
+	public decimal diff { get; set; }
+
+	/// <summary>거래량</summary>
+	public long volume { get; set; }
+}
---
+++ LsOpenApi/Models/T1866.cs
@@ -0,0 +1,61 @@
+﻿namespace LsOpenApi.Models;
+/// <summary>
+/// 서버저장조건리스트조회(API)(t1866)
+/// </summary>
+internal class t1866 : LsResponseCore
+{
+	public t1866InBlock t1866InBlock { get; set; } = new();
+	public t1866OutBlock t1866OutBlock { get; set; } = new();
+	public List<t1866OutBlock1> t1866OutBlock1 { get; set; } = new();
+}
+
+/// <summary>
+/// 서버저장조건리스트조회(API)(t1866) - InBlock
+/// </summary>
+internal class t1866InBlock
+{
+	/// <summary>로그인ID</summary>
+	public string user_id { get; set; } = string.Empty;
+
+	/// <summary>조회구분</summary>
+	public string gb { get; set; } = "0";
+
+	/// <summary>        그룹명</summary>
+	public string group_name { get; set; } = string.Empty;
+
+	/// <summary>연속여부</summary>
+	public string cont { get; set; } = "0";
+
+	/// <summary>        연속키</summary>
+	public string contkey { get; set; } = string.Empty;
+}
+
+/// <summary>
+/// 서버저장조건리스트조회(API)(t1866) - OutBlock
+/// </summary>
+internal class t1866OutBlock
+{
+	/// <summary>        저장조건수</summary>
+	public long result_count { get; set; }
+
+	/// <summary>연속여부</summary>
+	public string cont { get; set; } = string.Empty;
+
+	/// <summary>        연속키</summary>
+	public string contkey { get; set; } = string.Empty;
+}
+
+/// <summary>
+/// 서버저장조건리스트조회(API)(t1866) - OutBlock1
+/// </summary>
+internal class t1866OutBlock1
+{
+	/// <summary>        서버저장인덱스</summary>
+	public string query_index { get; set; } = string.Empty;
+
+	/// <summary>        그룹명</summary>
+	public string group_name { get; set; } = string.Empty;
+
+	/// <summary>        조건저장명</summary>
+	public string query_name { get; set; } = string.Empty;
+}
