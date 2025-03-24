c3be11c0dc8fceffa62fcd9ed6734b691d39e4ec
rudollee <rudollee@gmail.com>
2025-03-25 02:42:15 +0900

    ls krx futures connection implemented

--- LsOpenApi/KrxFutures/KrxFuturesConnection.cs
+++ LsOpenApi/KrxFutures/KrxFuturesConnection.cs
@@ -1,9 +1,49 @@
-﻿using OpenBroker;
+﻿using System.Net.WebSockets;
+using System.Text.Json;
+using OpenBroker;
 using OpenBroker.Models;
+using Websocket.Client;
 
 namespace LsOpenApi.KrxFutures;
 public partial class LsKrxFutures : ConnectionBase, IConnection
 {
-	public Task<ResponseCore> ConnectAsync() => throw new NotImplementedException();
 	public Task<ResponseResult<KeyPack>> RequestApprovalKeyAsync(string appkey, string secretkey) => throw new NotImplementedException();
+
+	public async Task<ResponseCore> ConnectAsync() => await ConnectAsync(Callback);
+
+	private void Callback(ResponseMessage message)
+	{
+		if (message is null || message.MessageType != WebSocketMessageType.Text)
+		{
+			Message(this, new ResponseCore
+			{
+				Broker = Brkr.LS,
+				Code = "BINARY",
+				Message = "binary message type"
+			});
+			return;
+		}
+
+		if (message.Text is null)
+		{
+			Message(this, new ResponseCore
+			{
+				Broker = Brkr.LS,
+				Code = "TEXTNULL",
+				Message = "message.Text is null"
+			});
+			return;
+		}
+
+		var document = JsonDocument.Parse(message.Text);
+		var root = document.RootElement;
+
+		var trCode = root.GetProperty("header").GetProperty("tr_cd").GetString();
+
+		switch (trCode)
+		{
+			default:
+				break;
+		}
+	}
 }
