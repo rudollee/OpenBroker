b6a323e260507112676b6bd44d8798a0108c54a5
rudollee <rudollee@gmail.com>
2025-04-14 12:12:55 +0900

    kis subscription callback saparated

--- KisOpenApi/KrxEquity/KrxEquityConnection.cs
+++ KisOpenApi/KrxEquity/KrxEquityConnection.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography;
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography;
 using System.Text;
 using KisOpenApi.Models;
 using OpenBroker;
@@ -32,122 +33,172 @@ public partial class KisKrxEquity : ConnectionBase, IConnection
 		var plainTxt = rawData[0] == "0" ? rawData[3] : decrypt(rawData[3]);
 
 		var data = plainTxt.Split("^");
-		switch (rawData[1])
-		{
-			#region 실시간 호가 H0STASP0
-			case nameof(H0STASP0):
-				if (OrderBookTaken is null) return;
+		if (data is null || !data.Any()) return;
 
-				var bidList = new List<MarketOrder>();
-				var askList = new List<MarketOrder>();
-				var bidQuantityIndex = (int)H0STASP0.BIDP_RSQN1;
-				var bidPriceIndex = (int)H0STASP0.BIDP1;
-				var askQuantityIndex = (int)H0STASP0.ASKP_RSQN1;
-				var askPriceIndex = (int)H0STASP0.ASKP1;
-				for (int i = 0; i < 10; i++)
+		var result = rawData[1] switch
+		{
+			nameof(H0STASP0) => CallbackH0STASP0(data),
+			nameof(H0STCNT0) => CallbackH0STCNT0(data),
+			nameof(H0STCNI0) => CallbackH0STCNI0(data),
+			_ => false
+		};
+	}
+	#endregion
+
+	#region 실시간 호가 H0STASP0 - callback
+	private bool CallbackH0STASP0(string[] data)
+	{
+		if (OrderBookTaken is null) return false;
+
+		try
+		{
+			var bidList = new List<MarketOrder>();
+			var askList = new List<MarketOrder>();
+			var bidQuantityIndex = (int)H0STASP0.BIDP_RSQN1;
+			var bidPriceIndex = (int)H0STASP0.BIDP1;
+			var askQuantityIndex = (int)H0STASP0.ASKP_RSQN1;
+			var askPriceIndex = (int)H0STASP0.ASKP1;
+			for (int i = 0; i < 10; i++)
+			{
+				bidList.Add(new MarketOrder
 				{
-					bidList.Add(new MarketOrder
-					{
-						Seq = Convert.ToByte(i + 1),
-						Amount = Convert.ToDecimal(data[bidQuantityIndex + i]),
-						Price = Convert.ToDecimal(data[bidPriceIndex + i]),
-					});
-					askList.Add(new MarketOrder
-					{
-						Seq = Convert.ToByte(i + 1),
-						Amount = Convert.ToDecimal(data[askQuantityIndex + i]),
-						Price = Convert.ToDecimal(data[askPriceIndex + i])
-					});
-				}
-				OrderBookTaken(this, new ResponseResult<OrderBook>
+					Seq = Convert.ToByte(i + 1),
+					Amount = Convert.ToDecimal(data[bidQuantityIndex + i]),
+					Price = Convert.ToDecimal(data[bidPriceIndex + i]),
+				});
+				askList.Add(new MarketOrder
 				{
-					Typ = MessageType.MKT,
-					Code = rawData[2],
-					Info = new OrderBook
+					Seq = Convert.ToByte(i + 1),
+					Amount = Convert.ToDecimal(data[askQuantityIndex + i]),
+					Price = Convert.ToDecimal(data[askPriceIndex + i])
+				});
+			}
+
+			OrderBookTaken(this, new ResponseResult<OrderBook>
+			{
+				Typ = MessageType.MKT,
+				Code = "001",
+				Info = new OrderBook
+				{
+					Ask = askList,
+					Bid = bidList,
+					AskAgg = askList.Sum(x => x.Amount),
+					BidAgg = bidList.Sum(x => x.Amount),
+					Symbol = data[(int)HDFFF010.series_cd],
+					TimeContract = (data[(int)HDFFF010.recv_date] + data[(int)HDFFF010.recv_time]).ToDateTimeMicro(),
+				},
+				Broker = Brkr.KI
+			});
+
+			return true;
+		}
+		catch (Exception ex)
+		{
+			OrderBookTaken(this, new ResponseResult<OrderBook>
+			{
+				StatusCode = Status.ERROR_OPEN_API,
+				Typ = MessageType.SYSERR,
+				Code = "ERR",
+				Message = ex.Message,
+			});
+
+			return false;
+		}
+	}
+	#endregion
+
+	#region 실시간 체결가 H0STCNT0 - callback
+	private bool CallbackH0STCNT0(string[] data)
+	{
+		if (MarketContracted is null) return false;
+		try
+		{
+			MarketContracted(this, new ResponseResult<MarketContract>
+			{
+				Typ = MessageType.MKT,
+				Code = "001",
+				Info = new MarketContract
+				{
+					MarketSessionInfo = data[(int)H0STCNT0.NEW_MKOP_CLS_CODE].Substring(0, 1) switch
 					{
-						Ask = askList,
-						Bid = bidList,
-						AskAgg = askList.Sum(x => x.Amount),
-						BidAgg = bidList.Sum(x => x.Amount),
-						Symbol = data[(int)HDFFF010.series_cd],
-						TimeContract = (data[(int)HDFFF010.recv_date] + data[(int)HDFFF010.recv_time]).ToDateTimeMicro(),
+						"1" => MarketSession.PRE,
+						"2" => MarketSession.REGULAR,
+						"3" => MarketSession.CLOSED,
+						"4" => MarketSession.AFTER,
+						_ => MarketSession.REGULAR
 					},
-					Remark = plainTxt,
-					Broker = Brkr.KI
-				});
-				break;
-			#endregion
-			#region 실시간 체결가 H0STCNT0
-			case nameof(H0STCNT0):
-				if (MarketContracted is null) return;
-				MarketContracted(this, new ResponseResult<MarketContract>
-				{
-					Typ = MessageType.MKT,
-					Code = rawData[2],
-					Info = new MarketContract
+					Symbol = data[(int)H0STCNT0.MKSC_SHRN_ISCD],
+					TimeContract = (DateTime.Now.ToString("yyyyMMdd") + data[(int)H0STCNT0.STCK_CNTG_HOUR]).ToDateTime(),
+					C = Convert.ToDecimal(data[(int)H0STCNT0.STCK_PRPR]),
+					V = Convert.ToDecimal(data[(int)H0STCNT0.CNTG_VOL]),
+					ContractSide = data[(int)H0STCNT0.CCLD_DVSN] switch
 					{
-						MarketSessionInfo = data[(int)H0STCNT0.NEW_MKOP_CLS_CODE].Substring(0, 1) switch
-						{
-							"1" => MarketSession.PRE,
-							"2" => MarketSession.REGULAR,
-							"3" => MarketSession.CLOSED,
-							"4" => MarketSession.AFTER,
-							_ => MarketSession.REGULAR
-						},
-						Symbol = data[(int)H0STCNT0.MKSC_SHRN_ISCD],
-						TimeContract = (DateTime.Now.ToString("yyyyMMdd") + data[(int)H0STCNT0.STCK_CNTG_HOUR]).ToDateTime(),
-						C = Convert.ToDecimal(data[(int)H0STCNT0.STCK_PRPR]),
-						V = Convert.ToDecimal(data[(int)H0STCNT0.CNTG_VOL]),
-						ContractSide = data[(int)H0STCNT0.CCLD_DVSN] switch
-						{
-							"1" => ContractSide.ASK,
-							"5" => ContractSide.BID,
-							_ => ContractSide.NONE
-						},
-						BasePrice = Convert.ToDecimal(data[(int)H0STCNT0.STCK_PRPR]) - Convert.ToDecimal(data[(int)H0STCNT0.PRDY_VRSS]),
-						VolumeAcc = Convert.ToDecimal(data[(int)H0STCNT0.ACML_VOL]),
-						MoneyAcc = Convert.ToDecimal(data[(int)H0STCNT0.ACML_TR_PBMN]) / 1_000_000,
+						"1" => ContractSide.ASK,
+						"5" => ContractSide.BID,
+						_ => ContractSide.NONE
 					},
-					Remark = plainTxt,
-					Broker = Brkr.KI
-				});
-				break;
-			#endregion
-			#region 실시간 체결 통보 H0STCNI0
-			case nameof(H0STCNI0):
-				Contracted(this, new ResponseResult<Contract>
+					BasePrice = Convert.ToDecimal(data[(int)H0STCNT0.STCK_PRPR]) - Convert.ToDecimal(data[(int)H0STCNT0.PRDY_VRSS]),
+					VolumeAcc = Convert.ToDecimal(data[(int)H0STCNT0.ACML_VOL]),
+					MoneyAcc = Convert.ToDecimal(data[(int)H0STCNT0.ACML_TR_PBMN]) / 1_000_000,
+				},
+				Broker = Brkr.KI
+			});
+
+			return true;
+		}
+		catch (Exception ex)
+		{
+			MarketContracted(this, new ResponseResult<MarketContract>
+			{
+				StatusCode = Status.ERROR_OPEN_API,
+				Typ = MessageType.SYSERR,
+				Code = "ERR",
+				Message = ex.Message,
+			});
+
+			return false;
+		}
+	}
+	#endregion
+
+	#region 실시간 체결 통보 H0STCNI0 - callback
+	private bool CallbackH0STCNI0(string[] data)
+	{
+		if (Contracted is null) return false;
+		try
+		{
+			Contracted(this, new ResponseResult<Contract>
+			{
+				Typ = MessageType.CONTRACT,
+				StatusCode = Status.SUCCESS,
+				Code = "001",
+				Info = new Contract
 				{
-					Typ = MessageType.CONTRACT,
-					StatusCode = Status.SUCCESS,
-					Code = rawData[2],
-					Info = new Contract
-					{
-						BrokerCo = "KI",
-						DateBiz = DateOnly.FromDateTime(DateTime.Now),
-						OID = Convert.ToInt64(data[(int)H0STCNI0.ODER_NO]),
-						CID = 0,
-						IsLong = data[(int)H0STCNI0.SELN_BYOV_CLS] == "02",
-						Price = Convert.ToDecimal(data[(int)H0STCNI0.CNTG_UNPR]),
-						Volume = Convert.ToInt32(data[(int)H0STCNI0.CNTG_QTY]),
-						TimeContracted = data[(int)H0STCNI0.STCK_CNTG_HOUR].ToDateTime(),
-						Symbol = data[(int)H0STCNI0.STCK_SHRN_ISCD],
-					},
-					Remark = plainTxt,
-					Broker = Brkr.KI
-				});
-				break;
-			#endregion
-			default:
-				Message(this, new ResponseCore
-				{
-					Typ = MessageType.SYSERR,
-					StatusCode = Status.ERROR_OPEN_API,
-					Code = rawData[2],
-					Message = "during parsing callback to swich",
-					Remark = plainTxt,
-					Broker = Brkr.KI
-				});
-				break;
+					BrokerCo = "KI",
+					DateBiz = DateOnly.FromDateTime(DateTime.Now),
+					OID = Convert.ToInt64(data[(int)H0STCNI0.ODER_NO]),
+					CID = 0,
+					IsLong = data[(int)H0STCNI0.SELN_BYOV_CLS] == "02",
+					Price = Convert.ToDecimal(data[(int)H0STCNI0.CNTG_UNPR]),
+					Volume = Convert.ToInt32(data[(int)H0STCNI0.CNTG_QTY]),
+					TimeContracted = data[(int)H0STCNI0.STCK_CNTG_HOUR].ToDateTime(),
+					Symbol = data[(int)H0STCNI0.STCK_SHRN_ISCD],
+				},
+				Broker = Brkr.KI
+			});
+
+			return true;
+		}
+		catch (Exception ex)
+		{
+			Contracted(this, new ResponseResult<Contract>
+			{
+				StatusCode = Status.ERROR_OPEN_API,
+				Typ = MessageType.SYSERR,
+				Code = "ERR",
+				Message = ex.Message,
+			});
+			return false;
 		}
 	}
 	#endregion
