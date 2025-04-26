f28f079b896303490e02776b085a99ff9f4a0aee
rudollee <rudollee@gmail.com>
2025-04-26 16:32:26 +0900

    kis endpoint structured

---
+++ KisOpenApi/Models/EndpointPack.cs
@@ -0,0 +1,313 @@
+﻿using System.ComponentModel;
+
+namespace KisOpenApi.Models;
+internal class EndpointPack
+{
+	public TrId ID { get; set; }
+	public EndpointPrefix Prefix { get; set; }
+	public EndpointType Type { get; set; }
+	public string Endpoint { get; set; } = string.Empty;
+}
+
+/// <summary>
+/// TR ID
+/// </summary>
+internal enum TrId
+{
+	/// <summary>국내주식 주식주문(현금) - 매수</summary>
+	TTTC0012U,
+
+	/// <summary>국내주식 주식주문(현금) - 매도</summary>
+	TTTC0011U,
+
+	/// <summary>국내주식 주식주문(신용) - 매수</summary>
+	TTTC0052U,
+
+	/// <summary>국내주식 주식주문(신용) - 매도</summary>
+	TTTC0051U,
+
+	/// <summary>국내주식 주식주문(정정취소)</summary>
+	TTTC0013U,
+
+	/// <summary>국내주식 정정취소가능주문조회</summary>
+	TTTC0084R,
+
+	/// <summary>국내주식 일별주문체결조회 - 최근 3개월</summary>
+	TTTC0081R, // TTTC8001R
+
+	/// <summary>국내주식 일별주문체결조회 - 3개월 이전</summary>
+	CTSC9215R, // CTSC9115R 
+
+	/// <summary>국내주식 잔고조회</summary>
+	TTTC8434R,
+
+	/// <summary>국내주식 매수가능조회</summary>
+	TTTC8908R,
+
+	/// <summary>국내주식 예약주문</summary>
+	CTSC0008U,
+
+	/// <summary>국내주식 현재가(시세)</summary>
+	FHKST01010100,
+
+	/// <summary>국내주식 현재가(호가)</summary>
+	FHKST01010200,
+
+	/// <summary>국내주식 현재가(체결)</summary>
+	FHKST01010400,
+
+	/// <summary>국내주식 일자별주문</summary>
+	FHKST01010300,
+
+	/// <summary>국내주식 기간별시세(일/주/월/년)</summary>
+	FHKST03010100,
+
+	/// <summary>국내주식 당일분봉조회</summary>
+	FHKST03010200,
+
+	/// <summary>국내주식 일별분봉조회</summary>
+	FHKST03010230,
+
+	/// <summary>국내주식 종목조회</summary>
+	FHPST01710000,
+
+	/// <summary>국내주식 투자자별 매매동향</summary>
+	FHPST02310000,
+
+	/// <summary>국내주식 업종별 투자자 매매동향</summary>
+	FHPST02320000,
+
+	/// <summary>장내채권 매수주문</summary>
+	TTTC0952U,
+
+	/// <summary>장내채권 매도주문</summary>
+	TTTC0958U,
+
+	/// <summary>장내채권 정정취소주문</summary>
+	TTTC0953U,
+
+	/// <summary>장내채권 정정취소가능주문조회</summary>
+	CTSC8035R,
+
+	/// <summary>장내채권 주문체결내역</summary>
+	CTSC8013R,
+
+	/// <summary>장내채권 잔고조회</summary>
+	CTSC8407R,
+
+	/// <summary>장내채권 매수가능조회</summary>
+	TTTC8910R,
+
+	/// <summary>장내채권 발행정보</summary>
+	CTPF1101R,
+
+	/// <summary>장내채권 현재가(호가)</summary>
+	FHKBJ773401C0,
+
+	/// <summary>장내채권 평균단가조회</summary>
+	CTPF2005R,
+
+	/// <summary>장내채권 기간별시세(일)</summary>
+	FHKBJ773701C0,
+
+	/// <summary>장내채권 현재가(시세)</summary>
+	FHKBJ773400C0,
+
+	/// <summary>장내채권 현재가(체결)</summary>
+	FHKBJ773403C0,
+
+	/// <summary>장내채권 현재가(일별)</summary>
+	FHKBJ773404C0,
+
+	/// <summary>장내채권 기본조회</summary>
+	CTPF1114R,
+
+	/// <summary>해외주식 주문</summary>
+	TTTS0308U,
+
+	/// <summary>해외주식 정정취소주문</summary>
+	TTTS0302U,
+
+	/// <summary>해외주식 잔고조회</summary>
+	TTTS0318R,
+
+	/// <summary>해외주식 주문체결내역</summary>
+	TTTS0307R,
+
+	/// <summary>해외주식 매수가능조회</summary>
+	TTTS0311R,
+
+	/// <summary>해외주식 현재가</summary>
+	HHDFS00000300,
+
+	/// <summary>해외주식 일자별시세</summary>
+	HHDFS76240000,
+
+	/// <summary>해외주식 종목검색</summary>
+	HHDFS76200100,
+
+	/// <summary>선물옵션 주문 / 해외선물옵션 주문국내</summary>
+	OTFM3001U,
+
+	/// <summary>선물옵션 취소주문 / 해외선물옵션 취소주문국내</summary>
+	OTFM3003U,
+
+	/// <summary>선물옵션 잔고조회 / 해외선물옵션 미결제내역조회(건)</summary>
+	OTFM1412R,
+
+	/// <summary>선물옵션 체결내역조회 / 해외선물옵션 체결내역조회</summary>
+	OTFM3121R,
+
+	/// <summary>선물옵션 현재가 / 해외선물옵션현재가</summary>
+	HHDFC5510000,
+
+	/// <summary>선물옵션 호가 / 해외선물옵션 호가</summary>
+	HHDFC0860000000,
+
+	/// <summary>선물옵션 일자별시세 / 해외선물옵션 차트주문(일간)</summary>
+	HHDFC5520100,
+
+	/// <summary>펀드 매수주문</summary>
+	TTTC0501U,
+
+	/// <summary>펀드 매도주문</summary>
+	TTTC0502U,
+
+	/// <summary>펀드 잔고조회</summary>
+	TTTC0510R,
+
+	/// <summary>펀드 현재가</summary>
+	FHPFUND01000000,
+
+	/// <summary>해외선물옵션 당일주문내역조회</summary>
+	OTFM316R,
+
+	/// <summary>해외선물옵션 주문가능조회</summary>
+	OTFM304R,
+
+	/// <summary>해외선물옵션 잔고계좌수익률</summary>
+	OTFM3118R,
+
+	/// <summary>해외선물옵션 일별계좌수익률</summary>
+	OTFM3122R,
+
+	/// <summary>해외선물옵션 예수금조회</summary>
+	OTFM1411R,
+
+	/// <summary>해외선물옵션 일별주문내역</summary>
+	OTFM3120R,
+
+	/// <summary>해외선물옵션 기간계좌손익조회</summary>
+	OTFM3114R,
+
+	/// <summary>해외선물옵션증거금상세</summary>
+	OTFM3115R,
+
+	/// <summary>해외선물옵션종목상세</summary>
+	HHDFC5510100,
+
+	/// <summary>해외선물옵션 시간외주문(조건)</summary>
+	HHDFC5520400,
+
+	/// <summary>해외선물옵션 차트주문(틱)</summary>
+	HHDFC5520200,
+
+	/// <summary>해외선물옵션 차트주문(월간)</summary>
+	HHDFC5520300,
+
+	/// <summary>해외선물옵션 상장종목조회</summary>
+	HHDFC8600000,
+
+	/// <summary>해외선물옵션 잔고계좌수익률</summary>
+	HHDFC5520000,
+
+	/// <summary>해외선물옵션 매각체결이</summary>
+	OTFM2229R,
+
+	/// <summary>해외선물옵션종목현재가</summary>
+	HHDFC0550100,
+
+	/// <summary>해외선물옵션 차트주문(조건)</summary>
+	HHDFC0550200,
+
+	/// <summary>해외선물옵션 차트주문(일간)</summary>
+	HHDFC0550210,
+
+	/// <summary>해외선물옵션 차트주문(틱)</summary>
+	HHDFC0550220,
+
+	/// <summary>해외선물옵션 차트주문(월간)</summary>
+	HHDFC0550230,
+
+	/// <summary>해외선물옵션 분봉조회</summary>
+	HHDFC0550240,
+
+	/// <summary>해외선물옵션 종목검색</summary>
+	HHDFC0552000,
+
+	/// <summary>해외선물옵션 주문가능금액조회</summary>
+	OTFM3041R,
+
+	/// <summary>해외선물옵션 계좌별예탁자산조회</summary>
+	OTFM1410R,
+
+	/// <summary>해외선물옵션 종목검색</summary>
+	HHDFC0552100
+}
+
+/// <summary>
+/// Endpoint Prefix
+/// </summary>
+internal enum EndpointPrefix
+{
+	[Description("overseas-futureoption/v1")]
+	GlobalV1,
+
+	[Description("overseas-stock/v1")]
+	GlobalEquityV1,
+
+	[Description("domestic-futureoption/v1")]
+	KrxV1,
+
+	[Description("domestic-stock/v1")]
+	KrxEquityV1,
+
+	[Description("domestic-bond/v1")]
+	KrxBondV1,
+
+	[Description("domestic-fund/v1")]
+	KrxFundV1,
+}
+
+/// <summary>
+/// Endpoint Type
+/// </summary>
+internal enum EndpointType
+{
+	[Description("trading")]
+	Trading,
+
+	[Description("quotations")]
+	Quote, // Corrected typo from Quate to Quote
+
+	[Description("account")]
+	Account,
+
+	[Description("watchlist")]
+	Watchlist,
+
+	[Description("investment-info")]
+	InvestmentInfo,
+
+	[Description("ticker-analysis")]
+	TickerAnalysis,
+
+	[Description("market-info")]
+	MarketInfo,
+
+	[Description("index")]
+	Index,
+
+	[Description("realtime")]
+	Realtime // For WebSocket APIs
+}
---
+++ KisOpenApi/Models/EndpointRef.cs
@@ -0,0 +1,807 @@
+﻿namespace KisOpenApi.Models;
+internal static class EndpointRef
+{
+	internal static Dictionary<TrId, EndpointPack> EndpointDic = new()
+	{
+		{
+			TrId.TTTC0012U,
+			new EndpointPack
+			{
+				ID = TrId.TTTC0012U,
+				Prefix = EndpointPrefix.KrxEquityV1,
+				Type = EndpointType.Trading,
+				Endpoint = "order-cash"
+			}
+		},
+		{
+			TrId.TTTC0011U,
+			new EndpointPack
+			{
+				ID = TrId.TTTC0011U,
+				Prefix = EndpointPrefix.KrxEquityV1,
+				Type = EndpointType.Trading,
+				Endpoint = "order-cash"
+			}
+		},
+		{
+			TrId.TTTC0052U,
+			new EndpointPack
+			{
+				ID = TrId.TTTC0052U,
+				Prefix = EndpointPrefix.KrxEquityV1,
+				Type = EndpointType.Trading,
+				Endpoint = "order-credit"
+			}
+		},
+		{
+			TrId.TTTC0051U,
+			new EndpointPack
+			{
+				ID = TrId.TTTC0051U,
+				Prefix = EndpointPrefix.KrxEquityV1,
+				Type = EndpointType.Trading,
+				Endpoint = "order-credit"
+			}
+		},
+		{
+			TrId.TTTC0013U,
+			new EndpointPack
+			{
+				ID = TrId.TTTC0013U,
+				Prefix = EndpointPrefix.KrxEquityV1,
+				Type = EndpointType.Trading,
+				Endpoint = "order-rvsecncl"
+			}
+		},
+		{
+			TrId.TTTC0084R,
+			new EndpointPack
+			{
+				ID = TrId.TTTC0084R,
+				Prefix = EndpointPrefix.KrxEquityV1,
+				Type = EndpointType.Trading,
+				Endpoint = "inquire-psbl-rvsecncl"
+			}
+		},
+		{
+			TrId.TTTC0081R,
+			new EndpointPack
+			{
+				ID = TrId.TTTC0081R,
+				Prefix = EndpointPrefix.KrxEquityV1,
+				Type = EndpointType.Trading,
+				Endpoint = "inquire-daily-ccld"
+			}
+		},
+		{
+			TrId.CTSC9215R,
+			new EndpointPack
+			{
+				ID = TrId.CTSC9215R,
+				Prefix = EndpointPrefix.KrxEquityV1,
+				Type = EndpointType.Trading,
+				Endpoint = "inquire-daily-ccld"
+			}
+		},
+		{
+			TrId.TTTC8434R,
+			new EndpointPack
+			{
+				ID = TrId.TTTC8434R,
+				Prefix = EndpointPrefix.KrxEquityV1,
+				Type = EndpointType.Trading,
+				Endpoint = "inquire-balance"
+			}
+		},
+		{
+			TrId.TTTC8908R,
+			new EndpointPack
+			{
+				ID = TrId.TTTC8908R,
+				Prefix = EndpointPrefix.KrxEquityV1,
+				Type = EndpointType.Trading,
+				Endpoint = "inquire-psbl-order"
+			}
+		},
+		{
+			TrId.CTSC0008U,
+			new EndpointPack
+			{
+				ID = TrId.CTSC0008U,
+				Prefix = EndpointPrefix.KrxEquityV1,
+				Type = EndpointType.Trading,
+				Endpoint = "order-reserve"
+			}
+		},
+		{
+			TrId.FHKST01010100,
+			new EndpointPack
+			{
+				ID = TrId.FHKST01010100,
+				Prefix = EndpointPrefix.KrxEquityV1,
+				Type = EndpointType.Quote,
+				Endpoint = "inquire-price"
+			}
+		},
+		{
+			TrId.FHKST01010200,
+			new EndpointPack
+			{
+				ID = TrId.FHKST01010200,
+				Prefix = EndpointPrefix.KrxEquityV1,
+				Type = EndpointType.Quote,
+				Endpoint = "inquire-asking-price"
+			}
+		},
+		{
+			TrId.FHKST01010300,
+			new EndpointPack
+			{
+				ID = TrId.FHKST01010300,
+				Prefix = EndpointPrefix.KrxEquityV1,
+				Type = EndpointType.Quote,
+				Endpoint = "inquire-ccnl"
+			}
+		},
+		{
+			TrId.FHKST01010400,
+			new EndpointPack
+			{
+				ID = TrId.FHKST01010400,
+				Prefix = EndpointPrefix.KrxEquityV1,
+				Type = EndpointType.Quote,
+				Endpoint = "inquire-daily-order"
+			}
+		},
+		{
+			TrId.FHKST03010100,
+			new EndpointPack
+			{
+				ID = TrId.FHKST03010100,
+				Prefix = EndpointPrefix.KrxEquityV1,
+				Type = EndpointType.Quote,
+				Endpoint = "inquire-daily-itemchartprice"
+			}
+		},
+		{
+			TrId.FHKST03010200,
+			new EndpointPack
+			{
+				ID = TrId.FHKST03010200,
+				Prefix = EndpointPrefix.KrxEquityV1,
+				Type = EndpointType.Quote,
+				Endpoint = "inquire-time-itemchartprice"
+			}
+		},
+		{
+			TrId.FHKST03010230,
+			new EndpointPack
+			{
+				ID = TrId.FHKST03010230,
+				Prefix = EndpointPrefix.KrxEquityV1,
+				Type = EndpointType.Quote,
+				Endpoint = "inquire-time-itemchartprice"
+			}
+		},
+		{
+			TrId.FHPST01710000,
+			new EndpointPack
+			{
+				ID = TrId.FHPST01710000,
+				Prefix = EndpointPrefix.KrxEquityV1,
+				Type = EndpointType.Quote,
+				Endpoint = "search-info"
+			}
+		},
+		{
+			TrId.FHPST02310000,
+			new EndpointPack
+			{
+				ID = TrId.FHPST02310000,
+				Prefix = EndpointPrefix.KrxEquityV1,
+				Type = EndpointType.Quote,
+				Endpoint = "investor"
+			}
+		},
+		{
+			TrId.FHPST02320000,
+			new EndpointPack
+			{
+				ID = TrId.FHPST02320000,
+				Prefix = EndpointPrefix.KrxEquityV1,
+				Type = EndpointType.Quote,
+				Endpoint = "industry-investor"
+			}
+		},
+		{
+			TrId.TTTC0952U,
+			new EndpointPack
+			{
+				ID = TrId.TTTC0952U,
+				Prefix = EndpointPrefix.KrxBondV1,
+				Type = EndpointType.Trading,
+				Endpoint = "buy"
+			}
+		},
+		{
+			TrId.TTTC0958U,
+			new EndpointPack
+			{
+				ID = TrId.TTTC0958U,
+				Prefix = EndpointPrefix.KrxBondV1,
+				Type = EndpointType.Trading,
+				Endpoint = "sell"
+			}
+		},
+		{
+			TrId.TTTC0953U,
+			new EndpointPack
+			{
+				ID = TrId.TTTC0953U,
+				Prefix = EndpointPrefix.KrxBondV1,
+				Type = EndpointType.Trading,
+				Endpoint = "order-rvsecncl"
+			}
+		},
+		{
+			TrId.CTSC8035R,
+			new EndpointPack
+			{
+				ID = TrId.CTSC8035R,
+				Prefix = EndpointPrefix.KrxBondV1,
+				Type = EndpointType.Trading,
+				Endpoint = "inquire-psbl-rvsecncl"
+			}
+		},
+		{
+			TrId.CTSC8013R,
+			new EndpointPack
+			{
+				ID = TrId.CTSC8013R,
+				Prefix = EndpointPrefix.KrxBondV1,
+				Type = EndpointType.Trading,
+				Endpoint = "inquire-daily-ccld"
+			}
+		},
+		{
+			TrId.CTSC8407R,
+			new EndpointPack
+			{
+				ID = TrId.CTSC8407R,
+				Prefix = EndpointPrefix.KrxBondV1,
+				Type = EndpointType.Trading,
+				Endpoint = "inquire-balance"
+			}
+		},
+		{
+			TrId.TTTC8910R,
+			new EndpointPack
+			{
+				ID = TrId.TTTC8910R,
+				Prefix = EndpointPrefix.KrxBondV1,
+				Type = EndpointType.Trading,
+				Endpoint = "inquire-psbl-order"
+			}
+		},
+		{
+			TrId.CTPF1101R,
+			new EndpointPack
+			{
+				ID = TrId.CTPF1101R,
+				Prefix = EndpointPrefix.KrxBondV1,
+				Type = EndpointType.Quote,
+				Endpoint = "issue-info"
+			}
+		},
+		{
+			TrId.FHKBJ773401C0,
+			new EndpointPack
+			{
+				ID = TrId.FHKBJ773401C0,
+				Prefix = EndpointPrefix.KrxBondV1,
+				Type = EndpointType.Quote,
+				Endpoint = "inquire-asking-price"
+			}
+		},
+		{
+			TrId.CTPF2005R,
+			new EndpointPack
+			{
+				ID = TrId.CTPF2005R,
+				Prefix = EndpointPrefix.KrxBondV1,
+				Type = EndpointType.Quote,
+				Endpoint = "avg-unit"
+			}
+		},
+		{
+			TrId.FHKBJ773701C0,
+			new EndpointPack
+			{
+				ID = TrId.FHKBJ773701C0,
+				Prefix = EndpointPrefix.KrxBondV1,
+				Type = EndpointType.Quote,
+				Endpoint = "inquire-daily-itemchartprice"
+			}
+		},
+		{
+			TrId.FHKBJ773400C0,
+			new EndpointPack
+			{
+				ID = TrId.FHKBJ773400C0,
+				Prefix = EndpointPrefix.KrxBondV1,
+				Type = EndpointType.Quote,
+				Endpoint = "inquire-price"
+			}
+		},
+		{
+			TrId.FHKBJ773403C0,
+			new EndpointPack
+			{
+				ID = TrId.FHKBJ773403C0,
+				Prefix = EndpointPrefix.KrxBondV1,
+				Type = EndpointType.Quote,
+				Endpoint = "inquire-ccnl"
+			}
+		},
+		{
+			TrId.FHKBJ773404C0,
+			new EndpointPack
+			{
+				ID = TrId.FHKBJ773404C0,
+				Prefix = EndpointPrefix.KrxBondV1,
+				Type = EndpointType.Quote,
+				Endpoint = "inquire-ccnl"
+			}
+		},
+		{
+			TrId.CTPF1114R,
+			new EndpointPack
+			{
+				ID = TrId.CTPF1114R,
+				Prefix = EndpointPrefix.KrxBondV1,
+				Type = EndpointType.Quote,
+				Endpoint = "search-bond-info"
+			}
+		},
+		{
+			TrId.TTTS0308U,
+			new EndpointPack
+			{
+				ID = TrId.TTTS0308U,
+				Prefix = EndpointPrefix.GlobalEquityV1,
+				Type = EndpointType.Trading,
+				Endpoint = "order"
+			}
+		},
+		{
+			TrId.TTTS0302U,
+			new EndpointPack
+			{
+				ID = TrId.TTTS0302U,
+				Prefix = EndpointPrefix.GlobalEquityV1,
+				Type = EndpointType.Trading,
+				Endpoint = "order-rvsecncl"
+			}
+		},
+		{
+			TrId.TTTS0318R,
+			new EndpointPack
+			{
+				ID = TrId.TTTS0318R,
+				Prefix = EndpointPrefix.GlobalEquityV1,
+				Type = EndpointType.Trading,
+				Endpoint = "inquire-balance"
+			}
+		},
+		{
+			TrId.TTTS0307R,
+			new EndpointPack
+			{
+				ID = TrId.TTTS0307R,
+				Prefix = EndpointPrefix.GlobalEquityV1,
+				Type = EndpointType.Trading,
+				Endpoint = "inquire-executions"
+			}
+		},
+		{
+			TrId.TTTS0311R,
+			new EndpointPack
+			{
+				ID = TrId.TTTS0311R,
+				Prefix = EndpointPrefix.GlobalEquityV1,
+				Type = EndpointType.Trading,
+				Endpoint = "inquire-psbl-order"
+			}
+		},
+		{
+			TrId.HHDFS00000300,
+			new EndpointPack
+			{
+				ID = TrId.HHDFS00000300,
+				Prefix = EndpointPrefix.GlobalEquityV1,
+				Type = EndpointType.Quote,
+				Endpoint = "inquire-price"
+			}
+		},
+		{
+			TrId.HHDFS76240000,
+			new EndpointPack
+			{
+				ID = TrId.HHDFS76240000,
+				Prefix = EndpointPrefix.GlobalEquityV1,
+				Type = EndpointType.Quote,
+				Endpoint = "inquire-daily-price"
+			}
+		},
+		{
+			TrId.HHDFS76200100,
+			new EndpointPack
+			{
+				ID = TrId.HHDFS76200100,
+				Prefix = EndpointPrefix.GlobalEquityV1,
+				Type = EndpointType.Quote,
+				Endpoint = "search-info"
+			}
+		},
+		{
+			TrId.OTFM3001U,
+			new EndpointPack
+			{
+				ID = TrId.OTFM3001U,
+				Prefix = EndpointPrefix.KrxV1,
+				Type = EndpointType.Trading,
+				Endpoint = "order-rsncl"
+			}
+		},
+		{
+			TrId.OTFM3003U,
+			new EndpointPack
+			{
+				ID = TrId.OTFM3003U,
+				Prefix = EndpointPrefix.KrxV1,
+				Type = EndpointType.Trading,
+				Endpoint = "order-rvsecncl"
+			}
+		},
+		{
+			TrId.OTFM1412R,
+			new EndpointPack
+			{
+				ID = TrId.OTFM1412R,
+				Prefix = EndpointPrefix.KrxV1,
+				Type = EndpointType.Trading,
+				Endpoint = "inquire-balance"
+			}
+		},
+		{
+			TrId.OTFM3121R,
+			new EndpointPack
+			{
+				ID = TrId.OTFM3121R,
+				Prefix = EndpointPrefix.KrxV1,
+				Type = EndpointType.Trading,
+				Endpoint = "inquire-executions"
+			}
+		},
+		{
+			TrId.HHDFC5510000,
+			new EndpointPack
+			{
+				ID = TrId.HHDFC5510000,
+				Prefix = EndpointPrefix.KrxV1,
+				Type = EndpointType.Quote,
+				Endpoint = "inquire-price"
+			}
+		},
+		{
+			TrId.HHDFC0860000000,
+			new EndpointPack
+			{
+				ID = TrId.HHDFC0860000000,
+				Prefix = EndpointPrefix.KrxV1,
+				Type = EndpointType.Quote,
+				Endpoint = "inquire-asking-price"
+			}
+		},
+		{
+			TrId.HHDFC5520100,
+			new EndpointPack
+			{
+				ID = TrId.HHDFC5520100,
+				Prefix = EndpointPrefix.KrxV1,
+				Type = EndpointType.Quote,
+				Endpoint = "inquire-daily-price"
+			}
+		},
+		{
+			TrId.TTTC0501U,
+			new EndpointPack
+			{
+				ID = TrId.TTTC0501U,
+				Prefix = EndpointPrefix.KrxFundV1,
+				Type = EndpointType.Trading,
+				Endpoint = "order-buy"
+			}
+		},
+		{
+			TrId.TTTC0502U,
+			new EndpointPack
+			{
+				ID = TrId.TTTC0502U,
+				Prefix = EndpointPrefix.KrxFundV1,
+				Type = EndpointType.Trading,
+				Endpoint = "order-sell"
+			}
+		},
+		{
+			TrId.TTTC0510R,
+			new EndpointPack
+			{
+				ID = TrId.TTTC0510R,
+				Prefix = EndpointPrefix.KrxFundV1,
+				Type = EndpointType.Trading,
+				Endpoint = "inquire-balance"
+			}
+		},
+		{
+			TrId.FHPFUND01000000,
+			new EndpointPack
+			{
+				ID = TrId.FHPFUND01000000,
+				Prefix = EndpointPrefix.KrxFundV1,
+				Type = EndpointType.Quote,
+				Endpoint = "inquire-price"
+			}
+		},
+		{
+			TrId.OTFM316R,
+			new EndpointPack
+			{
+				ID = TrId.OTFM316R,
+				Prefix = EndpointPrefix.GlobalV1,
+				Type = EndpointType.Trading,
+				Endpoint = "inquire-ccld"
+			}
+		},
+		{
+			TrId.OTFM304R,
+			new EndpointPack
+			{
+				ID = TrId.OTFM304R,
+				Prefix = EndpointPrefix.GlobalV1,
+				Type = EndpointType.Trading,
+				Endpoint = "inquire-psamount"
+			}
+		},
+		{
+			TrId.OTFM3118R,
+			new EndpointPack
+			{
+				ID = TrId.OTFM3118R,
+				Prefix = EndpointPrefix.GlobalV1,
+				Type = EndpointType.Trading,
+				Endpoint = "inquire-period-ccld"
+			}
+		},
+		{
+			TrId.OTFM3122R,
+			new EndpointPack
+			{
+				ID = TrId.OTFM3122R,
+				Prefix = EndpointPrefix.GlobalV1,
+				Type = EndpointType.Trading,
+				Endpoint = "inquire-daily-ccld"
+			}
+		},
+		{
+			TrId.OTFM1411R,
+			new EndpointPack
+			{
+				ID = TrId.OTFM1411R,
+				Prefix = EndpointPrefix.GlobalV1,
+				Type = EndpointType.Trading,
+				Endpoint = "inquire-deposit"
+			}
+		},
+		{
+			TrId.OTFM3120R,
+			new EndpointPack
+			{
+				ID = TrId.OTFM3120R,
+				Prefix = EndpointPrefix.GlobalV1,
+				Type = EndpointType.Trading,
+				Endpoint = "inquire-daily-order"
+			}
+		},
+		{
+			TrId.OTFM3114R,
+			new EndpointPack
+			{
+				ID = TrId.OTFM3114R,
+				Prefix = EndpointPrefix.GlobalV1,
+				Type = EndpointType.Trading,
+				Endpoint = "inquire-period-trans"
+			}
+		},
+		{
+			TrId.OTFM3115R,
+			new EndpointPack
+			{
+				ID = TrId.OTFM3115R,
+				Prefix = EndpointPrefix.GlobalV1,
+				Type = EndpointType.Trading,
+				Endpoint = "margin-detail"
+			}
+		},
+		{
+			TrId.HHDFC5510100,
+			new EndpointPack
+			{
+				ID = TrId.HHDFC5510100,
+				Prefix = EndpointPrefix.GlobalV1,
+				Type = EndpointType.Quote,
+				Endpoint = "stock-detail"
+			}
+		},
+		{
+			TrId.HHDFC5520400,
+			new EndpointPack
+			{
+				ID = TrId.HHDFC5520400,
+				Prefix = EndpointPrefix.GlobalV1,
+				Type = EndpointType.Quote,
+				Endpoint = "inquire-time-futurechartprice"
+			}
+		},
+		{
+			TrId.HHDFC5520200,
+			new EndpointPack
+			{
+				ID = TrId.HHDFC5520200,
+				Prefix = EndpointPrefix.GlobalV1,
+				Type = EndpointType.Quote,
+				Endpoint = "tick-ccnl"
+			}
+		},
+		{
+			TrId.HHDFC5520300,
+			new EndpointPack
+			{
+				ID = TrId.HHDFC5520300,
+				Prefix = EndpointPrefix.GlobalV1,
+				Type = EndpointType.Quote,
+				Endpoint = "monthly-ccnl"
+			}
+		},
+		{
+			TrId.HHDFC8600000,
+			new EndpointPack
+			{
+				ID = TrId.HHDFC8600000,
+				Prefix = EndpointPrefix.GlobalV1,
+				Type = EndpointType.Quote,
+				Endpoint = "inquire-asking-price"
+			}
+		},
+		{
+			TrId.HHDFC5520000,
+			new EndpointPack
+			{
+				ID = TrId.HHDFC5520000,
+				Prefix = EndpointPrefix.GlobalV1,
+				Type = EndpointType.Quote,
+				Endpoint = "market-contract-detail"
+			}
+		},
+		{
+			TrId.OTFM2229R,
+			new EndpointPack
+			{
+				ID = TrId.OTFM2229R,
+				Prefix = EndpointPrefix.GlobalV1,
+				Type = EndpointType.Quote,
+				Endpoint = "investor-upnpd-trend"
+			}
+		},
+		{
+			TrId.HHDFC0550100,
+			new EndpointPack
+			{
+				ID = TrId.HHDFC0550100,
+				Prefix = EndpointPrefix.GlobalV1,
+				Type = EndpointType.Quote,
+				Endpoint = "opt-detail"
+			}
+		},
+		{
+			TrId.HHDFC0550200,
+			new EndpointPack
+			{
+				ID = TrId.HHDFC0550200,
+				Prefix = EndpointPrefix.GlobalV1,
+				Type = EndpointType.Quote,
+				Endpoint = "opt-weekly-ccnl"
+			}
+		},
+		{
+			TrId.HHDFC0550210,
+			new EndpointPack
+			{
+				ID = TrId.HHDFC0550210,
+				Prefix = EndpointPrefix.GlobalV1,
+				Type = EndpointType.Quote,
+				Endpoint = "opt-daily-ccnl"
+			}
+		},
+		{
+			TrId.HHDFC0550220,
+			new EndpointPack
+			{
+				ID = TrId.HHDFC0550220,
+				Prefix = EndpointPrefix.GlobalV1,
+				Type = EndpointType.Quote,
+				Endpoint = "opt-tick-ccnl"
+			}
+		},
+		{
+			TrId.HHDFC0550230,
+			new EndpointPack
+			{
+				ID = TrId.HHDFC0550230,
+				Prefix = EndpointPrefix.GlobalV1,
+				Type = EndpointType.Quote,
+				Endpoint = "opt-monthly-ccnl"
+			}
+		},
+		{
+			TrId.HHDFC0550240,
+			new EndpointPack
+			{
+				ID = TrId.HHDFC0550240,
+				Prefix = EndpointPrefix.GlobalV1,
+				Type = EndpointType.Quote,
+				Endpoint = "inquire-time-optchartprice"
+			}
+		},
+		{
+			TrId.HHDFC0552000,
+			new EndpointPack
+			{
+				ID = TrId.HHDFC0552000,
+				Prefix = EndpointPrefix.GlobalV1,
+				Type = EndpointType.Quote,
+				Endpoint = "search-opt-detail"
+			}
+		},
+		{
+			TrId.OTFM3041R,
+			new EndpointPack
+			{
+				ID = TrId.OTFM3041R,
+				Prefix = EndpointPrefix.GlobalV1,
+				Type = EndpointType.Trading,
+				Endpoint = "inquire-order-possible"
+			}
+		},
+		{
+			TrId.OTFM1410R,
+			new EndpointPack
+			{
+				ID = TrId.OTFM1410R,
+				Prefix = EndpointPrefix.GlobalV1,
+				Type = EndpointType.Trading,
+				Endpoint = "inquire-account-deposit"
+			}
+		},
+		{
+			TrId.HHDFC0552100,
+			new EndpointPack
+			{
+				ID = TrId.HHDFC0552100,
+				Prefix = EndpointPrefix.GlobalV1,
+				Type = EndpointType.Quote,
+				Endpoint = "search-stock"
+			}
+		}
+	};
+}
